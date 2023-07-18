1b43eccd0a8778ab51e3ed00475825f982b9ee9c
Kenji Lamy <kenji.lamy@gmail.com>
2023-07-18 16:33:05 +0200

    webhook test

--- app.py
+++ app.py
@@ -33,15 +33,12 @@ def proba():
         pred = make_prediction(client_id).tolist()[0]
         return pred
 
-@app.route('/update_server', methods=['GET'])
+@app.route('/update_server', methods=['GET', 'POST'])
 def webhook():
-        if request.method == 'GET':
-            repo = git.Repo('../Projet7_scoring_model')
-            origin = repo.remotes.origin
-            origin.pull()
-            return 'Updated PythonAnywhere successfully', 200
-        else:
-             return 'Not working!'
+    repo = git.Repo('../Projet7_scoring_model')
+    origin = repo.remotes.origin
+    origin.pull()
+    return 'Updated PythonAnywhere successfully', 200
 
 'push test 4'
           
