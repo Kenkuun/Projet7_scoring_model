fe4803139c7ab8cafe6cbd4efb3f7de3a981c4f3
Kenji Lamy <kenji.lamy@gmail.com>
2023-07-19 14:47:49 +0200

    webhook test

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def proba():
 
 @app.route('/update_server', methods=['POST', 'GET'])
 def webhook():
-    if request.method == 'POST':
+    if request.method == 'GET':
         repo = git.Repo('../Projet7_scoring_model')
         origin = repo.remotes.origin
         # repo.create_head('master',
