53ba0cbb63e384725c6548bb78bed19eabd36089
Kenji Lamy <kenji.lamy@gmail.com>
2023-07-19 15:37:06 +0200

    webhook test

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def proba():
 
 @app.route('/update_server', methods=['POST', 'GET'])
 def webhook():
-    if request.method == 'GET':
+    if request.method == 'POST':
         repo = git.Repo('./')
         origin = repo.remotes.origin
         origin.pull()
