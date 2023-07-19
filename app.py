e22a40c7a35941c10ca39339cdb73dda9922a9ce
Kenji Lamy <kenji.lamy@gmail.com>
2023-07-19 14:40:03 +0200

    webhook test

--- app.py
+++ app.py
@@ -35,15 +35,15 @@ def proba():
 
 @app.route('/update_server', methods=['POST', 'GET'])
 def webhook():
-    if request.method == 'GET':
-        repo = git.Repo('../Projet7_scoring_model')
-        origin = repo.remotes.origin
-        repo.create_head('master',
-        origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-        origin.pull()
+    # if request.method == 'GET':
+    repo = git.Repo('../Projet7_scoring_model')
+    origin = repo.remotes.origin
+    repo.create_head('master',
+    origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
+    origin.pull()
         return 'Updated PythonAnywhere', 200
-    else:
-        return 'Not Working'
+    # else:
+    #     return 'Not Working'
 
 'push test 5'
           
