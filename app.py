d33a4e8248986db33f2a00dfe25a6b752d5eec13
Kenji Lamy <kenji.lamy@gmail.com>
2023-07-26 12:11:07 +0200

    test debug

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ def make_prediction(client_id):
 def proba():
     if 'client_id' in request.args:
         client_id = int(request.args["client_id"])
-        return client_id
-        # pred = make_prediction(client_id).tolist()[0]
-        # return pred
+        print("client ID is", client_id)
+        pred = make_prediction(client_id).tolist()[0]
+        return pred
 
 @app.route('/update_server', methods=['POST', 'GET'])
 def webhook():
